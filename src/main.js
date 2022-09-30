4c9b1c0aee7ce36e8b3b1055968082c17f96e807
lfatton <louise.fatton@hotmail.fr>
2022-09-30 12:52:17 +0200

    added twitter back instead of insta

--- src/main.js
+++ src/main.js
@@ -5,9 +5,9 @@ import router from "@/router"
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import { faSquareGithub, faLinkedin, faSquareInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faSquareGithub, faLinkedin, faSquareTwitter } from '@fortawesome/free-brands-svg-icons'
 
-library.add(faEnvelope, faSquareGithub, faLinkedin, faSquareInstagram)
+library.add(faEnvelope, faSquareGithub, faLinkedin, faSquareTwitter)
 
 const app = createApp(App)
 app.use(router);
